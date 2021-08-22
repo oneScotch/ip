35111152506492578e0c1a4fe512a7f11380644c
LAPTOP-V2NPSHUB\19656 <1965637476@qq.com>
2021-08-22 20:20:46 +0800

    level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static String line = "____________________________________________________________";
+
+    public static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,11 +11,25 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String line = "____________________________________________________________";
-
-        // Level 0_Greets the user
         System.out.println(line + "\nHello! I'm Duke\nWhat can I do for you?\n" + line);
-        // Exit
+    }
+
+    public static void exit() {
         System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+
+    public static void level1() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if (input.equals("bye")) {
+            exit();
+            return;
+        }
+        System.out.println(line + "\n" + input + "\n" + line);
+    }
+
+    public static void main(String[] args) {
+        greet();
+        level1();
     }
 }
