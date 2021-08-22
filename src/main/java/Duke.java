342e11c49e78e28fb5c344d5d5a32c50dd4a870f
LAPTOP-V2NPSHUB\19656 <1965637476@qq.com>
2021-08-22 13:01:26 +0800

    Level 0. Greet
    simply greets the user and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String line = "____________________________________________________________";
+
+        // Level 0_Greets the user
+        System.out.println(line + "\nHello! I'm Duke\nWhat can I do for you?\n" + line);
+        // Exit
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
     }
 }
